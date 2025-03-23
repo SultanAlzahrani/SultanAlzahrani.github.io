b3123881de6749a4d8b13e2cfb3dad463ecf436a
SultanAlzahrani <sultanalzahranics@gmail.com>
2025-03-23 19:57:56 +0300

    General Updates

---
+++ MemoryCardGame/src/components/array.jsx
@@ -0,0 +1,129 @@
+const mainArray = [
+  {
+    charSrc: "/characters/GGAC_Axl_Portrait.webp",
+    nameSrc: "/NamePlate/GGACR_Axl_Low_Nameplate.png",
+    id: 1,
+  },
+  {
+    charSrc: "/characters/GGAC_ABA_Portrait.webp",
+    nameSrc: "/NamePlate/GGACR_A.B.A_Nameplate.png",
+    id: 2,
+  },
+  {
+    charSrc: "/characters/GGAC_Anji_Portrait.webp",
+    nameSrc: "/NamePlate/GGACR_Anji_Mito_Nameplate.png",
+    id: 3,
+  },
+  {
+    charSrc: "/characters/GGAC_Baiken_Portrait.webp",
+    nameSrc: "/NamePlate/GGACR_Baiken_Nameplate.png",
+    id: 4,
+  },
+  {
+    charSrc: "/characters/GGAC_Bridget_Portrait.webp",
+    nameSrc: "/NamePlate/GGACR_Bridget_Nameplate.png",
+    id: 5,
+  },
+  {
+    charSrc: "/characters/GGAC_Chipp_Portrait.webp",
+    nameSrc: "/NamePlate/GGACR_Chipp_Zanuff_Nameplate.png",
+    id: 6,
+  },
+  {
+    charSrc: "/characters/GGAC_Dizzy_Portrait.webp",
+    nameSrc: "/NamePlate/GGACR_Dizzy_Nameplate.png",
+    id: 7,
+  },
+  {
+    charSrc: "/characters/GGAC_Eddie_Portrait.webp",
+    nameSrc: "/NamePlate/GGACR_Eddie_Nameplate.png",
+    id: 8,
+  },
+  {
+    charSrc: "/characters/GGAC_Faust_Portrait.webp",
+    nameSrc: "/NamePlate/GGACR_Faust_Nameplate.png",
+    id: 9,
+  },
+  {
+    charSrc: "/characters/GGAC_I-No_Portrait.webp",
+    nameSrc: "/NamePlate/GGACR_I-No_Nameplate.png",
+    id: 10,
+  },
+  {
+    charSrc: "/characters/GGAC_Jam_Portrait.webp",
+    nameSrc: "/NamePlate/GGACR_Jam_Kuradoberi_Nameplate.png",
+    id: 11,
+  },
+  {
+    charSrc: "/characters/GGAC_Johnny_Portrait.webp",
+    nameSrc: "/NamePlate/GGACR_Johnny_Nameplate.png",
+    id: 12,
+  },
+  {
+    charSrc: "/characters/GGAC_Justice_Portrait.webp",
+    nameSrc: "/NamePlate/GGACR_Justice_Nameplate.png",
+    id: 13,
+  },
+  {
+    charSrc: "/characters/GGAC_May_Portrait.webp",
+    nameSrc: "/NamePlate/GGACR_May_Nameplate.png",
+    id: 14,
+  },
+  {
+    charSrc: "/characters/GGAC_Millia_Portrait.webp",
+    nameSrc: "/NamePlate/GGACR_Millia_Rage_Nameplate.png",
+    id: 15,
+  },
+  {
+    charSrc: "/characters/GGAC_Order-Sol_Portrait.webp",
+    nameSrc: "/NamePlate/GGACR_Order-Sol_Nameplate.png",
+    id: 16,
+  },
+  {
+    charSrc: "/characters/GGAC_Potemkin_Portrait.webp",
+    nameSrc: "/NamePlate/GGACR_Potemkin_Nameplate.png",
+    id: 17,
+  },
+  {
+    charSrc: "/characters/GGAC_Slayer_Portrait.webp",
+    nameSrc: "/NamePlate/GGACR_Slayer_Nameplate.png",
+    id: 18,
+  },
+  {
+    charSrc: "/characters/GGAC_Testament_Portrait.webp",
+    nameSrc: "/NamePlate/GGACR_Testament_Nameplate.png",
+    id: 19,
+  },
+  {
+    charSrc: "/characters/GGAC_Zappa_Portrait.webp",
+    nameSrc: "/NamePlate/GGACR_Zappa_Nameplate.png",
+    id: 20,
+  },
+  {
+    charSrc: "/characters/GGACR_Kliff_Undersn_Portrait.webp",
+    nameSrc: "/NamePlate/GGACR_Kliff_Nameplate.png",
+    id: 21,
+  },
+  {
+    charSrc: "/characters/GGACR_Ky_Portrait.webp",
+    nameSrc: "/NamePlate/GGACR_Ky_Nameplate.png",
+    id: 22,
+  },
+  {
+    charSrc: "/characters/GGACR_Robo-Ky_Portrait.webp",
+    nameSrc: "/NamePlate/GGACR_Robo-Ky_Nameplate.png",
+    id: 23,
+  },
+  {
+    charSrc: "/characters/GGACR_Sol_Badguy_Portrait.webp",
+    nameSrc: "/NamePlate/GGACR_Sol_Nameplate.png",
+    id: 24,
+  },
+  {
+    charSrc: "/characters/GGACR_Venom_Portrait.webp",
+    nameSrc: "/NamePlate/GGACR_Venom_Nameplate.png",
+    id: 25,
+  },
+];
+
+export default mainArray;
